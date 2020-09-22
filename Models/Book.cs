a8f7d5c5054ee4658b25c7529c11cf816d8a56c3
josefina-r-casanova <josefina.r.casanova@ibm.com>
2020-09-22 10:00:19 -0300

    Primer commit

---
+++ Models/Book.cs
@@ -0,0 +1,7 @@
+public class Book
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public bool Read { get; set; }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace _5_Lab1.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
